ca66874b30bcc788844691d379be63bb3a51cd91
Venkat Ram Taddi <tadivenkat@gmail.com>
2024-01-15 12:29:37 -0600

    First commit

---
+++ src/main/java/com/venkat/unitTestingProject/UnitTestingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.venkat.unitTestingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestingProjectApplication.class, args);
+	}
+
+}
