08618cd2dcd1411f33ef23977cf4cf6f2fb3c279
Venkat Ram Taddi <tadivenkat@gmail.com>
2024-03-31 08:45:43 -0500

    Added a method to find nth term of fibonacci sequence. Also added unit tests

--- src/main/java/com/venkat/unitTestingProject/utils/Utils.java
+++ src/main/java/com/venkat/unitTestingProject/utils/Utils.java
@@ -4,6 +4,7 @@ import java.math.BigInteger;
 
 public class Utils {
     private Utils() {}
+    
     public static BigInteger getFactorial(Long number) {
         if (number <= 1) {
             return BigInteger.valueOf(1L);
@@ -13,5 +14,23 @@ public class Utils {
             result = result.multiply(BigInteger.valueOf(i));
         }
         return result;
+    }
+
+    public static BigInteger getNthTermOfFibonacciSequence(Long n) {
+        BigInteger timinus2 = BigInteger.valueOf(0);
+        BigInteger timinus1 = BigInteger.valueOf(1);
+        BigInteger ti = null;
+        if (n == 1) {
+            return timinus2;
+        }
+        if (n == 2) {
+            return timinus1;
+        }
+        for (long i = 3; i <= n; i++) {
+            ti = timinus2.add(timinus1);
+            timinus2 = timinus1;
+            timinus1 = ti;
+        }
+        return ti;
     }
 }
--- src/test/java/com/venkat/unitTestingProject/utils/UtilsTest.java
+++ src/test/java/com/venkat/unitTestingProject/utils/UtilsTest.java
@@ -2,12 +2,28 @@ package com.venkat.unitTestingProject.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigInteger;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UtilsTest {
+
+    @BeforeAll
+    static void init() {
+        System.out.println("Initializtion");
+    }
+
+    @BeforeEach
+    void setup() {
+        System.out.println("Setup something before each test");
+    }
+
     @Test
     void getFactorial_simpleTest() {
         BigInteger result = Utils.getFactorial(5L);
@@ -30,5 +46,51 @@ public class UtilsTest {
     void getFactorial_factorialOfNegativeNumberIs1Test() {
         BigInteger result = Utils.getFactorial(-2L);
         assertTrue(1L == result.longValue());
+    }
+
+    @Test
+    void getNthTermOfFibonacciSequence_firstTerm() {
+        BigInteger result = Utils.getNthTermOfFibonacciSequence(1L);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @Test
+    void getNthTermOfFibonacciSequence_secondTerm() {
+        BigInteger result = Utils.getNthTermOfFibonacciSequence(2L);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void getNthTermOfFibonacciSequence_thirdTerm() {
+        BigInteger result = Utils.getNthTermOfFibonacciSequence(3L);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void getNthTermOfFibonacciSequence_fourthTerm() {
+        BigInteger result = Utils.getNthTermOfFibonacciSequence(4L);
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    void getNthTermOfFibonacciSequence_eighthTerm() {
+        BigInteger result = Utils.getNthTermOfFibonacciSequence(8L);
+        assertThat(result).isEqualTo(13);
+    }
+
+    @Test
+    void getNthTermOfFibonacciSequence_thirtythTerm() {
+        BigInteger result = Utils.getNthTermOfFibonacciSequence(30L);
+        assertThat(result).isEqualTo(514229);
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("Cleanup something after each test");
+    }
+
+    @AfterAll
+    static void cleanupAfterAllTests() {
+        System.out.println("Cleanup called after execution of all the tests");
     }
 }
