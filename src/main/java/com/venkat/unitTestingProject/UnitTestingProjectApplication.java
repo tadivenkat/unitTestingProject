c50b76e349f139194f360f0eafe6bbf49c746c76
Venkat Ram Taddi <tadivenkat@gmail.com>
2024-04-20 14:08:49 -0500

    Converting Spring application into a stand alone application

--- src/main/java/com/venkat/unitTestingProject/UnitTestingProjectApplication.java
+++ src/main/java/com/venkat/unitTestingProject/UnitTestingProjectApplication.java
@@ -1,13 +1,10 @@
 package com.venkat.unitTestingProject;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class UnitTestingProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UnitTestingProjectApplication.class, args);
+		System.out.println("Hello World!!!");
 	}
 
 }
